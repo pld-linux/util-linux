4867a31120081ff1b3f9733b3b4f411f2b0dfdc3
Artur Frysiak <artur@frysiak.net>
1999-07-27 09:55:16 +0000

    polish man page
    Changed files:
        chkdupexe.1.pl -> 1.1

---
+++ chkdupexe.1.pl
@@ -0,0 +1,39 @@
+.\" {PTM/PB/0.1/28-09-1998/"znajd¼ dubluj±ce siê pliki wykonywalne"}
+.\" chkdupexe.1 -- 
+.\" Created: Sat Mar 11 18:19:44 1995 by faith@cs.unc.edu
+.\" Revised: Sat Mar 11 19:07:05 1995 by faith@cs.unc.edu
+.\" Revised: Wed Jul  5 01:56:26 1995 by shields@tembel.org
+.\" Copyright 1995 Rickard E. Faith (faith@cs.unc.edu)
+.\" 
+.\" Permission is granted to make and distribute verbatim copies of this
+.\" manual provided the copyright notice and this permission notice are
+.\" preserved on all copies.
+.\" 
+.\" Permission is granted to copy and distribute modified versions of this
+.\" manual under the conditions for verbatim copying, provided that the
+.\" entire resulting derived work is distributed under the terms of a
+.\" permission notice identical to this one
+.\" 
+.\" Since the Linux kernel and libraries are constantly changing, this
+.\" manual page may be incorrect or out-of-date.  The author(s) assume no
+.\" responsibility for errors or omissions, or for damages resulting from
+.\" the use of the information contained herein.  The author(s) may not
+.\" have taken the same level of care in the production of this manual,
+.\" which is licensed free of charge, as they might when working
+.\" professionally.
+.\" 
+.\" Formatted or processed versions of this manual, if unaccompanied by
+.\" the source, must acknowledge the copyright and authors of this work.
+.\" 
+.TH CHKDUPEXE 1 "11 Mar 1995" "" "Podrêcznik programisty linuxowego"
+.SH NAZWA
+chkdupexe \- znajd¼ dubluj±ce siê pliki wykonywalne
+.SH SK£ADNIA
+.B chkdupexe
+.SH OPIS
+.B chkdupexe
+przeszuka uniê $PATH i wkodowane na sztywno najpopularniejsze lokacje
+binariów. W raporcie wypisze wisz±ce symlinki i binaria o zduplikowanych
+nazwach.
+.SH AUTOR
+Nicolai Langfeldt, Michael Shields.
